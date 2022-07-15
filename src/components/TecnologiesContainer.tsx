ba004917683d29c2ff7d6042714ae88f7ba6e44c
Matheus <94717377+Mati-Pereira@users.noreply.github.com>
2022-07-15 02:13:52 -0300

    base pronta

--- src/components/TecnologiesContainer.tsx
+++ src/components/TecnologiesContainer.tsx
@@ -5,6 +5,9 @@ import {
   DiNodejsSmall,
   DiReact
 } from 'react-icons/di'
+import {
+  SiTypescript
+} from 'react-icons/si'
 
 import '../styles/components/tecnologies.scss'
 
@@ -12,6 +15,7 @@ const tecnologies = [
   { id: "html", name: "HTML5", icon: <DiHtml5 /> },
   { id: "css", name: "CSS", icon: <DiCss3 /> },
   { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+  { id: "ts", name: "TypeScript", icon: <SiTypescript />},
   { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
   { id: "react", name: "React", icon: <DiReact /> }
 ]
@@ -20,15 +24,19 @@ function TecnologiesContainer() {
   return (
     <section id="tecnologies-container">
       <h2>Tecnologias</h2>
-      {tecnologies.map((tecnology, index) => (
-        <div id={tecnology.id} className="tecnology-card" key={index}>
-          {tecnology.icon}
-          <div className="tecnology-info">
-            <h3>{tecnology.name}</h3>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis nemo voluptatem cupiditate tenetur hic repellendus laborum, odit amet nihil sunt rerum suscipit, quos officia nostrum sint beatae distinctio excepturi nisi.</p>
+      <div className="tecnologies-grid">
+        {tecnologies.map((tecnology, index) => (
+          <div id={tecnology.id} className="tecnology-card" key={index}>
+            <div className="icon">
+              {tecnology.icon}
+            </div>
+            <div className="tecnology-info">
+              <h3>{tecnology.name}</h3>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis nemo voluptatem cupiditate </p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 }
